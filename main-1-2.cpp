705df90d7b92a1c04e05840cfbcc87278c4d5349
Amey Chande <YOUR_EMAIL>
2024-08-16 17:16:51 +0930

    2

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -6,11 +6,12 @@ using namespace std;
 int main() {
     int n = 3;
     PersonList personList = createPersonList(n);
+    
+    cout << "Number of people: " << personList.numPeople << endl;
 
-    std::cout << "Number of people: " << personList.numPeople << std::endl;
+    
     for (int i = 0; i < n; ++i) {
-        cout << "Person " << i + 1 << ": Name - " << personList.people[i].name
-             << ", Age - " << personList.people[i].age << endl;
+        cout << "Person " << i + 1 << ": Name - " << personList.people[i].name << ", Age - " << personList.people[i].age << endl;
     }
 
     delete[] personList.people;
