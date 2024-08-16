c72f683451ab0c575229fe7999ad7693ef9c4bdd
Amey Chande <YOUR_EMAIL>
2024-08-16 17:46:25 +0930

    1

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,16 +1,22 @@
+// function-1-2.cpp
 #include "Person.h"
-#include <iostream>
 
 PersonList createPersonList(int n) {
-    PersonList list;
-    list.numPeople = n;
-    list.people = new Person[n]; 
 
+    PersonList list;
+    
+    list.numPeople = n;
+    
+    list.people = new Person[n]; 
 
     for (int i = 0; i < n; ++i) {
         list.people[i].name = "Jane Doe";
         list.people[i].age = 1;
     }
+
+
+
+
 
     return list;
 }
--- main-1-3.cpp
+++ main-1-3.cpp
@@ -7,16 +7,16 @@ int main() {
     PersonList originalList = createPersonList(num);
     PersonList copiedList = deepCopyPersonList(originalList);
 
-    cout << "Original List:" << std::endl;
+    cout << "Original List:" << endl;
+    
     for (int i = 0; i < originalList.numPeople; ++i) {
-        cout << "Person " << i + 1 << ": Name = " << originalList.people[i].name
-                  << ", Age = " << originalList.people[i].age << endl;
+        cout << "Person " << i + 1 << ": Name = " << originalList.people[i].name << ", Age = " << originalList.people[i].age << endl;
     }
 
-    cout << "Copied List:" << std::endl;
+    cout << "Copied List:" << endl;
+
     for (int i = 0; i < copiedList.numPeople; ++i) {
-        cout << "Person " << i + 1 << ": Name = " << copiedList.people[i].name
-                  << ", Age = " << copiedList.people[i].age << endl;
+        cout << "Person " << i + 1 << ": Name = " << copiedList.people[i].name << ", Age = " << copiedList.people[i].age << endl;
     }
 
     delete[] originalList.people;
