d5acd6ba9ceeffd008728a17e44665a9bff56d54
Raymond Wen <raymondwen@Raymonds-MacBook-Air.local>
2024-10-12 00:19:52 +1030

    main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
-#include <iostream> 
-#include <string> 
-
+#include <iostream>
+#include <fstream>
 using namespace std;
+int main(){
+    ifstream fin;
 
+    fin.open("data.txt");
 
-int main() {
-    // Create a bank
-    Bank myBank("Global Bank", 1, {}, {});
+    if(!fin) {
+        cerr << "Error in opening the file" << endl;
+        return 1; // if this is main
+    }
+
+    static const char* const FILENAME_PEOPLE = "people.txt";
+
     
-    //
-    return 0;
+    ofstream fout;
+    fout.open(FILENAME_PEOPLE); // be sure that the argument is a c string
+    if (!fout) {
+        cerr << "Error in opening the output file" << endl;
+
+        // again only if this is main, chain return codes or throw an exception otherwise
+        return 1; 
+    }
+
 }
