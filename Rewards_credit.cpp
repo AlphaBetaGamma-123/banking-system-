cd025e50a762de342b9f31d278c30563ff529660
Amey Chande <YOUR_EMAIL>
2024-10-11 23:10:47 +1030

    rewards

--- Rewards_credit.cpp
+++ Rewards_credit.cpp
@@ -6,6 +6,7 @@ using namespace std;
 // Class RewardsCredit (inherits from CreditAccount)
 class RewardsCredit : public CreditAccount {
 protected:
+    //Datamembers. 
     int points;
     double points_rate;
     int points_limit;
@@ -18,17 +19,22 @@ public:
     RewardsCredit(double bal, double funds, double limit, double rate, int pts, double pts_rate, int pts_limit)
         : CreditAccount(bal, funds, limit, rate), points(pts), points_rate(pts_rate), points_limit(pts_limit) {}
 
-    // Add points based on transaction
+    //Adding points on the transaction.
     void add_points(double amount) {
         points += static_cast<int>(amount * points_rate);
     }
 
-    // Redeem points
+    // Redeem points.
     void redeem_points(int pts) {
         if (pts <= points) {
             points -= pts;
-            cout << "Redeemed " << pts << " points!\n";
-        } else {
-            cout << "Not enough points to redeem!\n";
+            cout << "Redeemed " << pts << "points\n";
+
+        }else{
+
+            cout << "Not enough points to reddem\n";
+
         }
-};
+    };
+
+}
